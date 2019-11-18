1891ccad006c97c8e99681a6462fa666413fc658
yekon <max1901@live.fr>
2019-11-18 11:12:49 +0100

    border lvl + lvl

--- app/containers/ApiRiot/PagePerso.js
+++ app/containers/ApiRiot/PagePerso.js
@@ -20,9 +20,6 @@ const useStyles = makeStyles(theme => ({
 export default function SimpleCard(props) {
   const { profil } = props
   const classes = useStyles()
-  const test = 4
-
-  console.log('profil data: ', profil)
 
   return (
     <div className={classes.root}>
@@ -30,8 +27,30 @@ export default function SimpleCard(props) {
       <Grid item xs={6}>
         <div className='profileContainer'>
           <img alt='profile Icon' className='imgIcon' src={`http://ddragon.leagueoflegends.com/cdn/9.21.1/img/profileicon/${profil.profileIconId}.png`}/>
-
-          <img alt='Profile Icon Border' className='imgBorder' src={`https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-${test}-solid-border.png`} />
+          <img alt='Profile Icon Border' className='imgBorder' src={
+            profil.summonerLevel <= 29  ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-1-solid-border.png' :
+            profil.summonerLevel <= 49 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-2-solid-border.png' : 
+            profil.summonerLevel <= 74 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-3-solid-border.png' :
+            profil.summonerLevel <= 99 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-4-solid-border.png' :
+            profil.summonerLevel <= 124 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-5-solid-border.png' : 
+            profil.summonerLevel <= 149 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-6-solid-border.png' : 
+            profil.summonerLevel <= 174 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-7-solid-border.png' : 
+            profil.summonerLevel <= 199 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-8-solid-border.png' : 
+            profil.summonerLevel <= 224  ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-9-solid-border.png' :
+            profil.summonerLevel <= 249 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-10-solid-border.png' : 
+            profil.summonerLevel <= 274 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-11-solid-border.png' :
+            profil.summonerLevel <= 299 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-12-solid-border.png' :
+            profil.summonerLevel <= 324 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-13-solid-border.png' : 
+            profil.summonerLevel <= 349 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-14-solid-border.png' : 
+            profil.summonerLevel <= 374 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-15-solid-border.png' : 
+            profil.summonerLevel <= 399 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-16-solid-border.png' :
+            profil.summonerLevel <= 424 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-17-solid-border.png' :
+            profil.summonerLevel <= 449 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-18-solid-border.png' :
+            profil.summonerLevel <= 474 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-19-solid-border.png' :
+            profil.summonerLevel <= 499 ? 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-20-solid-border.png' :
+            'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-uikit/global/default/images/theme-21-solid-border.png'
+          }/>
+          <p className='lvlBorder'>{profil.summonerLevel}</p>
         </div>
       </Grid>
       <Grid item xs={6}>
