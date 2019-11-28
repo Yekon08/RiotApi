b80ff08802dbd31aecf29f86e91e7a7dabb11319
yekon <max1901@live.fr>
2019-11-28 14:49:01 +0100

    Item for last game

--- app/containers/ApiRiot/ItemsHisto.js
+++ app/containers/ApiRiot/ItemsHisto.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { number } from 'prop-types'
+
+import './ItemsHisto.css'
 
 // API Settings
 const myheader = new Headers ({
@@ -16,14 +17,15 @@ export default class ItemsHisto extends React.Component {
         super(props)
         this.state = {
             gameId: [],
-            participantId: Number
+            participantId: Number,
+            itemsImg: []
         }
         console.log('itemsHisto: ', this.props.matchData.matches)
         console.log('nom: ', this.props.summonerName)
     }
 
     componentDidMount() {
-        const url = `http://ec2-52-47-60-225.eu-west-3.compute.amazonaws.com/lol/match/v4/matches/${this.props.matchData.matches[0].gameId}?platform=EUW1`
+        const url = `http://ec2-52-47-60-225.eu-west-3.compute.amazonaws.com/lol/match/v4/matches/${this.props.matchData.matches[4].gameId}?platform=EUW1`
         fetch(url, init)
             .then(response => response.json())
             .then(json => {
@@ -36,54 +38,72 @@ export default class ItemsHisto extends React.Component {
             .catch(error => console.log(error)) // error api
     }
 
+    componentDidUpdate(prevProps) {
+        if(prevProps.matchData !== this.props.matchData) {
+            console.log('update')
+            const url = `http://ec2-52-47-60-225.eu-west-3.compute.amazonaws.com/lol/match/v4/matches/${this.props.matchData.matches[4].gameId}?platform=EUW1`
+            fetch(url, init)
+                .then(response => response.json())
+                .then(json => {
+                    const data = json
+                    console.log('gameId : ', data)
+                    this.setState({ gameId: data })
+                    this.test1()
+                })
+                .catch(error => console.log(error)) // error json
+                .catch(error => console.log(error)) // error api
+        }
+    }
+
     test1 = () => {
-        console.log('ParticipantID 0 ', this.state.gameId.participants[0].participantId) // Participant ID
         console.log('participant identities: ', this.state.gameId.participantIdentities)
 
         this.state.gameId.participantIdentities.map((info) => {
             console.log('infos: ', info.player.summonerName)
 
             if(info.player.summonerName == this.props.summonerName) {
-                console.log('ok :', info.participantId)
                 this.setState({ participantId: info.participantId })
             }
         })
 
         console.log('Participant ID :', this.state.participantId)
+        this.test2()
     }
 
-    render() {
-
-        const tables = []
+    test2 = () => {
         if(this.state.participantId >= 0){
             this.state.gameId.participants.map((info) => {
-                console.log('stats: ', info.championId)
-    
                 if(info.participantId == this.state.participantId) {
-                    tables.push(
+                    this.setState({
+                        itemsImg: [
                         info.stats.item0,
                         info.stats.item1,
                         info.stats.item2,
                         info.stats.item3,
                         info.stats.item4,
                         info.stats.item5,
-                        info.stats.item6,
-                    )
+                        info.stats.item6 ]
+                    })
                 }
             })
         }
+        console.log('State img item: ', this.state.itemsImg)
+    }
 
-        console.log('tableaux items: ', tables)
+    render() {
 
-        let items = tables.map((item) => {
-            return (
-                <img src={`http://ddragon.leagueoflegends.com/cdn/9.23.1/img/item/${item}.png`} />
-            )
+        let items = this.state.itemsImg.map((item) => {
+            console.log('item: ', item)
+            if(item == undefined) {
+                return  <div className="noItem"></div> 
+            } else {
+                return <img src={`http://ddragon.leagueoflegends.com/cdn/9.23.1/img/item/${item}.png`} />
+            }
+
         })
 
         return(
             <div className="itemsHisto">
-                <p>test Items</p>
                 {items}
             </div>
         )
